05725de291110d558d86c426b23900fe5dbb0d4d
Shyam sundar R <shyamsundarr10@gmail.com>
2023-07-25 14:00:12 +0530

    index.js is created

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.get("/",(req,res)=>{
+    res.status(200).send("I created backend endpoint sucessfully")
+})
+app.listen(PORT,()=>console.log(`Server started in localhost : ${PORT}`))
