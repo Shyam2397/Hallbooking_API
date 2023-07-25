70fec7cbdf3eb99db8d75bbab8ffc7d3d63fea74
Shyam sundar R <shyamsundarr10@gmail.com>
2023-07-25 14:08:02 +0530

    import cors

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+
 const app = express();
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.use(cors());
 
 app.get("/",(req,res)=>{
     res.status(200).send("I created API endpoint sucessfully")
